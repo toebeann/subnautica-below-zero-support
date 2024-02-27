ecebd0a0219cbdb4fa9a7a99365209ae33301ffb
Tobey Blaber <contact@tobey.me>
2024-02-27 13:54:15 +0000

    Remove fake stores and refactor dev console init

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,10 @@ import '@total-typescript/ts-reset';
 import { join } from 'path';
 import { version } from '../package.json';
 import { BEPINEX_MOD_PATH, validateBepInEx } from './bepinex';
-import { migrateVersion, parseChangelog, validateChangelog } from './changelog';
+import { migrateVersion, parseChangelog, showChangelog, store as changelogStore, validateChangelog } from './changelog';
 import { EXTENSION_ID, GAME_EXE, GAME_NAME, UNITY_PLAYER } from './constants';
 import { QMM_MOD_DIR, validateQModManager } from './qmodmanager';
-import { getDiscovery, getModPath, getMods } from './utils';
+import { getDiscovery, getModPath, getMods, inspectAssembly } from './utils';
 import registerInstallerBepInEx from './installers/bepinex';
 import registerInstallerBepInExMixed from './installers/bepinex-mixed';
 import registerInstallerBepInExPatcher from './installers/bepinex-patcher';
@@ -44,7 +44,7 @@ import { NEXUS_GAME_ID } from './platforms/nexus';
 import { STEAM_GAME_ID } from './platforms/steam';
 import { XBOX_GAME_ID, getAppExecName } from './platforms/xbox';
 import { getFileVersion, getProductVersion } from 'exe-version';
-import { major, prerelease } from 'semver';
+import { major } from 'semver';
 import store2 from 'store2';
 import { actions, fs, selectors, types, util } from 'vortex-api';
 import { z } from 'zod';
@@ -56,15 +56,11 @@ import IExtensionApi = types.IExtensionApi;
 import IExtensionContext = types.IExtensionContext;
 import IGame = types.IGame;
 import GameStoreHelper = util.GameStoreHelper;
+import getSafe = util.getSafe;
 
 export const store = store2.namespace(EXTENSION_ID).namespace(`v${major(version, true)}`);
-store.isFake(prerelease(version, true)?.[0].toString() === 'dev');
 
 export default function main(context: IExtensionContext): boolean {
-    if (store.isFake()) {
-        debugSetup(context);
-    }
-
     context.registerMigration(migrateVersion);
 
     // register Subnautica: Below Zero with Vortex
@@ -118,6 +114,8 @@ export default function main(context: IExtensionContext): boolean {
     });
 
     context.once(async () => {
+        initDevConsole(context);
+
         context.api.events.on('gamemode-activated', async (gameMode: string) => {
             if (gameMode !== NEXUS_GAME_ID) {
                 return;
@@ -158,24 +156,50 @@ export default function main(context: IExtensionContext): boolean {
     return true;
 }
 
-const debugSetup = (context: IExtensionContext) => {
-    Object.assign(globalThis, {
-        toebean: {
-            sbz: {
-                context,
-                getState: () => context.api.getState(),
-                getDiscovery: () => getDiscovery(context.api.getState()),
-                getMods: () => getMods(context.api.getState()),
-                'vortex-api': {
-                    actions,
-                    selectors,
-                    types,
-                    util,
-                },
-                parseChangelog,
-            },
+const initDevConsole = (context: IExtensionContext) => {
+    const setDevConsole = (oldValue: unknown, newValue: unknown) => {
+        if (newValue && typeof newValue === 'object' && 'userId' in newValue && newValue.userId === 83851883) {
+            Object.assign(globalThis, {
+                subnauticaSupport: {
+                    context,
+                    getState: () => context.api.getState(),
+                    getDiscovery: (state = context.api.getState()) => getDiscovery(state),
+                    getMods: (state = context.api.getState()) => getMods(state),
+                    vortex: {
+                        actions,
+                        selectors,
+                        types,
+                        util,
+                    },
+                    stores: {
+                        main: store,
+                        changelog: changelogStore,
+                    },
+                    parseChangelog,
+                    showChangelog: async (changelog?: string | Awaited<ReturnType<typeof parseChangelog>>) => {
+                        switch (typeof changelog) {
+                            case 'string':
+                                changelog = await parseChangelog(changelog);
+                                break;
+                            case 'undefined':
+                                changelog = await parseChangelog();
+                                break;
+                        }
+
+                        return await showChangelog(context.api, changelog.html, changelog.releases);
+                    },
+                    inspectAssembly: (path: string, additionalSearchPaths: string[] = []) => inspectAssembly(context.api, path, undefined, additionalSearchPaths),
+                }
+            });
+        } else if ('subnauticaSupport' in globalThis &&
+            oldValue && typeof oldValue === 'object' && 'userId' in oldValue && oldValue.userId === 83851883) {
+            // @ts-expect-error
+            delete globalThis.subnauticaSupport;
         }
-    });
+    };
+
+    setDevConsole(undefined, getSafe(context.api.getState(), ['persistent', 'nexus', 'userInfo'], undefined));
+    context.api.onStateChange?.(['persistent', 'nexus', 'userInfo'], setDevConsole);
 }
 
 /**
